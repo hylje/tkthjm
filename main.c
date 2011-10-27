f188fb58f5c0168e190bf9a60d90c4467fc07960
Leo Honkanen <leo.o.honkanen@gmail.com>
2011-10-27 12:53:55 +0300

    new main.c

--- main.c
+++ main.c
@@ -1,3 +1,29 @@
-int main(int argc, const char* argv[]) {
-  return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include "functions.h"
+
+void readFileName(char* filename) {
+    printf("Please enter filename: ");
+    scanf("%s", filename);
+}
+
+void mainloop(FILE* file) {
+    //DO SOMETHING
+}
+
+int main(int argc, char** argv) {
+    char filename[50];
+    FILE* file = 0;
+    if (argc < 2) {
+        readFileName(filename);
+        file = fopen(filename, "r");
+    } else {
+        file = fopen(argv[1], "r");
+    }
+    if (file == NULL) {
+        puts("File could not be read");
+        exit(EXIT_FAILURE);
+    }
+    mainloop(file);
+    return (EXIT_SUCCESS);
 }
