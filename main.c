5a7b3aeaf9729e726c353a4d4a9e2f5bfeedcc1e
Juuso Hyvönen <juuso.hyvonen@gmail.com>
2011-10-29 16:34:24 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,30 +4,38 @@
 #include "functions.h"
 
 /**
- * Tekijä: Juuso Hyvönen
- * Tarkoitus: Parsii komentoriviparametrit sekä kutsuu pääohjelmaa
- * Parametrit: Parametrien lukumäärä, parametritaulukko.
- * -h tulostaa käyttöohjeet. Muussa tapauksessa parametrin
+ * Tekijï¿½: Juuso Hyvï¿½nen
+ * Tarkoitus: Parsii komentoriviparametrit sekï¿½ kutsuu pï¿½ï¿½ohjelmaa
+ * Parametrit: Parametrien lukumï¿½ï¿½rï¿½, parametritaulukko.
+ * -h tulostaa kï¿½yttï¿½ohjeet. Muussa tapauksessa parametrin
  * oletetaan olevan tiedostonimi
  * Palauttaa: exit-koodin
- * Muuttaa: Ei mitään-
+ * Muuttaa: Ei mitï¿½ï¿½n-
  */
 int main(int argc, char** argv) {
     FILE* infile = 0;
     FILE* outfile = 0;
 
+    // -h flag for help
     if (argc == 2 && strcmp(argv[1], "-h") == 0) {
       printHelp();
       return (EXIT_SUCCESS);
     }
 
+    // No Parameters
     if (argc == 1) {
         infile = stdin;
 	outfile = stdout;
-    } else if (argc == 2) {
+    }
+    
+    // One parameter
+    else if (argc == 2) {
       infile = fopen(argv[1], "r");
       outfile = stdout;
-    } else if (argc == 3) {
+    }
+    
+    // Two parameters
+    else if (argc == 3) {
       if (strcmp(argv[1], "-") == 0) {
 	infile = stdin;
       } else {
@@ -41,7 +49,7 @@ int main(int argc, char** argv) {
       return (EXIT_FAILURE);
     }
     if (outfile == NULL) {
-      puts("Input file could not be written to\n");
+      puts("Output file could not be written to\n");
       return (EXIT_FAILURE);
     }
 
